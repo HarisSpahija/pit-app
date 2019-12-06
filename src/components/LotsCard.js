acb8d6a4d38b0edf97fe72fd440859040e793977
Bart Janssen <bart.janssen1@pon.com>
2019-12-06 13:10:45 +0100

    Adjusted lots

--- src/components/LotsCard.js
+++ src/components/LotsCard.js
@@ -1,9 +1,14 @@
 import React from "react";
 import axios from "axios";
 
-import data from "./mock/lots";
+import data from "../mock/lots";
 import ParkingCard from "./ParkingCard";
-import parseJson from "parse-json";
+import Lambo from "../static/lambo.png";
+import Bentley from "../static/bentley.png";
+import Porsche from "../static/porsche.png";
+import Logistics from "../static/logistics.png";
+import Academy from "../static/academy.png";
+import Bezoekers from "../static/bezoekers.png";
 
 export default class LotsCard extends React.Component {
 
@@ -25,22 +30,54 @@ export default class LotsCard extends React.Component {
 
     render() {
         const { error, isLoaded, lots } = this.state;
+
+        const logosCard1 = [Lambo, Bentley, Porsche];
+        const logosCard2 = [Logistics];
+        const logosCard3 = [Academy];
+        const logosCard4 = [Bezoekers];
+
         if (error) {
             return <div>An error occurred!</div>;
         } else if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
 
-
-
             return (
-                <ul>
-                    {lots.map(item => (
-                        <li key={item.name}>
-                            {item.name}
-                        </li>
-                    ))}
-                </ul>
+                <div>
+
+                    <div class="row" style={{ height: "45vh" }}>
+                        <ParkingCard
+                            status="full"
+                            parkingLot={lots[0].id}
+                            parkingLotText={lots[0].name}
+                            logos={logosCard1}
+                            spotsAvailable={0}
+                        />
+                        <ParkingCard
+                            status="almost"
+                            parkingLot={lots[1].id}
+                            parkingLotText={lots[1].name}
+                            logos={logosCard2}
+                            spotsAvailable={3}
+                        />
+                    </div>
+                    <div class="row" style={{ height: "45vh" }}>
+                        <ParkingCard
+                            status="free"
+                            parkingLot={lots[2].id}
+                            parkingLotText={lots[2].name}
+                            logos={logosCard3}
+                            spotsAvailable={13}
+                        />
+                        <ParkingCard
+                            status="free"
+                            parkingLot={lots[3].name}
+                            parkingLotText={lots[3].name}
+                            logos={logosCard4}
+                            spotsAvailable={9}
+                        />
+                    </div>
+                </div>
             );
         }
     }
